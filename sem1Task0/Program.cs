207984751111b0db15aee9f8b235927064dffcf3
artem <normandysr777@yandex.ru>
2023-01-24 10:39:38 +0300

    homework1

---
+++ sem1Task0/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2){
+    Console.Write(number1 + " больше " + number2);
+}
+else if (number2 > number1){
+    Console.Write(number1 + " меньше " + number2);
+}
+else {
+    Console.Write("Введенные числа равны");
+}
