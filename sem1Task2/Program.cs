aa05962d514aec233e0b3d8d80240c190004d3f8
artem <normandysr777@yandex.ru>
2023-01-24 10:40:34 +0300

    homework1.0

--- sem1Task2/Program.cs
+++ sem1Task2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0){
+    Console.Write("Введено четное число");
+}
+else {
+    Console.Write("Введено нечетное число");
+}
