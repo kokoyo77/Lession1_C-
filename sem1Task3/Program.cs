f8a61bb0b9fb3bfe9a87078fde3b5c80fd21c3f6
artem <normandysr777@yandex.ru>
2023-01-24 10:43:57 +0300

    homework1.1

--- sem1Task3/Program.cs
+++ sem1Task3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while (current <= number){
+    Console.Write(current + " ");
+    current = current + 2;
+}
